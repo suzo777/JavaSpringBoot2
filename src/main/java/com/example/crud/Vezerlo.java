2c06653029bdafee54e4625544d121c670fb2f48
suzo777 <73218354+suzo777@users.noreply.github.com>
2022-09-07 12:38:15 +0200

    Update Vezerlo.java

--- src/main/java/com/example/crud/Vezerlo.java
+++ src/main/java/com/example/crud/Vezerlo.java
@@ -1,5 +1,6 @@
 package com.example.crud;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@Component
 public class Vezerlo {
     @Autowired
     private DolgRepo dolgRepo;
