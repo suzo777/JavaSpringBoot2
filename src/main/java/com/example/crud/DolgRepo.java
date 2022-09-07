b6c8ecd30b595649647bcdd6956946f72c5ec1c6
suzo777 <73218354+suzo777@users.noreply.github.com>
2022-09-07 12:20:34 +0200

    Update DolgRepo.java

--- src/main/java/com/example/crud/DolgRepo.java
+++ src/main/java/com/example/crud/DolgRepo.java
@@ -1,4 +1,7 @@
 package com.example.crud;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface DolgRepo extends CrudRepository<Dolgozo, Integer> {
 }
