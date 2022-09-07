83b32629226673a539d50d797b09e59afef4d136
suzo777 <73218354+suzo777@users.noreply.github.com>
2022-09-07 12:13:37 +0200

    Update CrudApplication.java

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -1,7 +1,9 @@
 package com.example.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 public class CrudApplication {
 
 	public static void main(String[] args) {
